4b322f524851449818635c1b8859a70e46de200f
bacmel <duarte.hugo@lilo.org>
2021-01-22 11:54:25 +0100

    finish path following

--- path_following/src/path_following.cpp
+++ path_following/src/path_following.cpp
@@ -3,7 +3,7 @@
 #include <nav_msgs/GetMap.h>
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/Twist.h>
 #include <nav_msgs/Path.h>
 #include <time.h>
 #include <stdio.h>
@@ -15,16 +15,21 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "commande");
     ros::NodeHandle n;
 
-    tf2_ros::Buffer tfBuffer;
-    tf2_ros::TransformListener tfListener(tfBuffer);
     Commande cmd;
     Pose_2D p;
-    ros::Subscriber sub = n.subscribe("rrt_path", 1, cmd.init);
+    ros::Subscriber sub_path = n.subscribe("rrt_path", 1, cmd.init);
+    ros::Publisher pub_cmd = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
 	while(!cmd.fin())
 	{
-		ros::Subscriber sub = n.subscribe("tf2", 1, p.init);
-		vector<double> v = cmd.commande_law(p.x(), p.y(), p.theta())
+		ros::Subscriber sub_tf = n.subscribe("tf", 1, p.init);
+		vector<double> v = cmd.commande_law(p.x(), p.y(), p.theta());
+		geometry_msgs::Twist t;
+		t.linear.x = v.at(0);
+		t.angular.z = v.at(1); 
+		pub_cmd.publish(t);
 	}
+	geometry_msgs::Twist t;
+	pub_cmd.publish(t);
 
 
     
